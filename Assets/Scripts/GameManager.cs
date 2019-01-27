4c73c7cdeb53a29a18803e64f605d96c1748998f
Damien <valenciow12344@gmail.com>
2019-01-27 16:05:36 -0500

    Committing Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    #region Timers
+
+    public int clockTimer;
+    public float currentTime;
+    public int endTime;
+    public float timer;
+    public float secondsPassedTimer;
+    #endregion
+
+    public float sanity = 100;
+    public float paranoia = 100;
+
+    public Image bG;
+
+    public Image sanityBar;
+    public Image paranoiaBar;
+
+    public GameObject bed;
+
+    public Animator bedAnimator;
+    public Animation[] dealAnimations;
+
+    public Animator blackScreen;
+
+    public TMP_Text paranoiaTimer;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    
+    void Start()
+    {
+        currentTime = 0f;
+        endTime = 600;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        secondsPassedTimer = currentTime;
+        currentTime = Mathf.RoundToInt(timer % 60);
+        paranoiaTimer.text = string.Format("Time Remaining: \n{0} / {1} ", currentTime, endTime);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            sanity -= 100;
+        }
+
+        if(Input.GetKey(KeyCode.Alpha0))
+        {
+            paranoiaTimer.gameObject.SetActive(true);
+        }
+        else
+        {
+            paranoiaTimer.gameObject.SetActive(false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            paranoia += 100;
+        }
+
+        if(secondsPassedTimer < currentTime)
+        {
+            paranoia -= 0.5f;
+        }
+
+        secondsPassedTimer = currentTime - 1;
+
+        UpdateBars();
+
+        EndGame();
+        //Debug.Log(string.Format("Seconds passed: {0}", currentTime));
+
+    }
+
+    void EndGame()
+    {
+        //If you've survived for 5 minutes, end the game.
+        if (currentTime >= endTime)
+        {
+            StartCoroutine(fadeScreen(1));
+        }
+
+        if (paranoia >= 100)
+        {
+            StartCoroutine(fadeScreen(2));
+            Debug.Log("Oh no paranoia");
+        }
+
+        if (sanity <= 0)
+        {
+            StartCoroutine(fadeScreen(3));
+            Debug.Log("Oh no sanity");
+        }
+    }
+
+    void UpdateBars()
+    {
+        sanityBar.fillAmount = Mathf.InverseLerp(100, 0, sanity);
+        paranoiaBar.fillAmount = Mathf.InverseLerp(0, 100, paranoia);
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        sanity -= damage;
+    }
+
+    IEnumerator fadeScreen(int endScreen)
+    {
+        blackScreen.Play("FadeAnim");
+
+        yield return new WaitForSeconds(1.2f);
+
+        SceneManager.LoadScene(endScreen);
+
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    Animator animator;
+
+    string myName;
+
+    float timer = 0;
+
+    int health = 100;
+
+    SpriteRenderer mySprite;
+
+    AudioSource myAudio;
+
+    #region Change in inspector
+    public int sanityDamage;
+
+    public float threat;
+
+    public float minThreatPerRate;
+    public float maxThreatPerRate;
+
+    public float threatRate = 5.5f;
+    public float sanityRate = 0.2f;
+    #endregion
+
+    float defeatTimer = 7;
+
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        myAudio = GetComponent<AudioSource>();
+        myName = gameObject.name;
+        threat = 0;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > threatRate)
+        {
+            threat += Mathf.RoundToInt(Random.Range(minThreatPerRate, maxThreatPerRate));
+            timer = 0;
+
+            //Debug.Log(string.Format("Threat is currently at {0}", threat));
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("THREAT 4"))
+        {
+            GameManager.instance.sanity -= sanityRate;
+            
+        }
+
+        if(health <= 10)
+        {
+            StartCoroutine(SetHealthBack());
+            threat = 0;
+        }
+
+        //After an X amount of time generate threat
+
+        //After reaching a threat threshold execute
+
+        //If clicked on and a minor amount of threat is generated you are considered 
+
+        //PARANOID!!!!!!!!
+
+        animator.SetFloat("Threat", threat);
+        animator.SetInteger("Health", health);
+
+    }
+
+    void OnMouseDown()
+    {
+
+        if(threat == 100)
+        {
+            GameManager.instance.TakeDamage(sanityDamage);
+        }
+
+        if(threat <= 40)
+        {
+            GameManager.instance.paranoia += 20;
+        }
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("THREAT 4"))
+        {
+            health -= 10;
+            StartCoroutine(DamageFlasher());
+        }
+
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("THREAT 4"))
+        {
+            DealWithYourParanoia();
+        }
+
+    }
+
+    public void DealWithYourParanoia()
+    {
+        threat = 0;
+
+        GameManager.instance.bedAnimator.SetTrigger("Deal with " + myName);
+    }
+
+    IEnumerator DamageFlasher()
+    {
+        mySprite.color = Color.red;
+        yield return null;
+        mySprite.color = Color.white;
+
+    }
+
+    IEnumerator SetHealthBack()
+    {
+        yield return new WaitForSeconds(defeatTimer);
+        health = 100;
+    }
+
+    public void playAudio()
+    {
+        myAudio.PlayOneShot(myAudio.clip);
+    }
+
+    public void stopAudio()
+    {
+        myAudio.Stop();
+
+    }
+}
